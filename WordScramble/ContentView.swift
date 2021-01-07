eba4f4aea1ed7ed48b8c6b713b12204498390900
Lime5005 <zlp5005@gmail.com>
2021-01-07 14:26:47 +0100

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,129 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Lime on 01/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var totalScore = 0
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button(action: {
+                    self.startGame()
+                    usedWords = [String]()
+                }) {
+                    Text("Change word")
+                }
+                TextField ("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    .padding()
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+                Text("You've got \(totalScore)" + "\(totalScore > 1 ? " points" : " point")")
+            }
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+    
+    func addNewWord() {
+        let answer =
+            newWord
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word")
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        totalScore += 1
+        newWord = ""
+    }
+    
+    func startGame() {
+        totalScore = 0
+        if let startWordsURL =
+            Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try?
+                String(contentsOf: startWordsURL) {
+                let allWords =
+                    startWords
+                    .components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord.lowercased()
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Lime on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
